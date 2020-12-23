db9bba79cdbed3dfcd63aafe7f974c7b838220a1
Eliud Berlin <info@eliud.io>
2020-12-23 17:39:15 +0100

    ready with subcollections

---
+++ test/eliud_pkg_workflow_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:eliud_pkg_workflow/eliud_pkg_workflow.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
