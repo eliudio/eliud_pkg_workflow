2a154ec8f8ac01d20f79d7a86d58c743d3350fdc
Eliud <58745656+eliudio@users.noreply.github.com>
2021-10-20 09:51:06 +0100

    Performance, reworked accessbloc and usage of accessbloc

--- lib/tools/action/workflow_action_model.dart
+++ lib/tools/action/workflow_action_model.dart
@@ -1,16 +1,18 @@
 import 'package:eliud_core/model/conditions_model.dart';
+import 'package:eliud_core/style/frontend/has_text.dart';
 import 'package:eliud_core/tools/action/action_entity.dart';
 import 'package:eliud_core/tools/action/action_model.dart';
 import 'package:eliud_core/tools/common_tools.dart';
 import 'package:eliud_pkg_workflow/model/abstract_repository_singleton.dart';
 import 'package:eliud_pkg_workflow/model/workflow_model.dart';
 import 'package:eliud_pkg_workflow/tools/action/workflow_action_entity.dart';
+import 'package:flutter/material.dart';
 
 // Start a workflow
 class WorkflowActionModel extends ActionModel {
   final WorkflowModel? workflow;
 
-  WorkflowActionModel(String? appId, { this.workflow, ConditionsModel? conditions} ) : super(appId, actionType: WorkflowActionEntity.label, conditions: conditions);
+  WorkflowActionModel(String appId, { this.workflow, ConditionsModel? conditions} ) : super(appId, actionType: WorkflowActionEntity.label, conditions: conditions);
 
   @override
   WorkflowActionEntity toEntity({String? appId}) {
@@ -22,12 +24,16 @@ class WorkflowActionModel extends ActionModel {
 
   static WorkflowActionModel? fromEntity(WorkflowActionEntity? entity) {
     if (entity == null) return null;
+    if (entity.appID == null) throw Exception('entity WorkflowActionModel.appID is null');
     return WorkflowActionModel(
-      entity.appID,
+      entity.appID!,
       conditions: ConditionsModel.fromEntity(entity.conditions),
     );
   }
+
   static Future<WorkflowActionModel?> fromEntityPlus(WorkflowActionEntity? entity, { String? appId}) async {
+    if (entity == null) return null;
+    if (entity.appID == null) throw Exception('entity WorkflowActionModel.appID is null');
     WorkflowModel? workFlowModel;
     if (entity == null) return null;
     if (entity.workflowId != null) {
@@ -39,7 +45,7 @@ class WorkflowActionModel extends ActionModel {
     }
 
     return WorkflowActionModel(
-        entity.appID,
+        entity.appID!,
         conditions: ConditionsModel.fromEntity(entity.conditions),
         workflow: workFlowModel
     );
@@ -47,6 +53,12 @@ class WorkflowActionModel extends ActionModel {
 
   String message() {
     return "Workflow";
+  }
+
+  @override
+  Widget describe(BuildContext context) {
+    if (workflow == null) return text(context, 'Incorrect workflow');
+    return text(context, 'Run workflow, ID = '  + (workflow!.documentID ?? 'no ID') + ', name = '+ (workflow!.name ?? 'no name'));
   }
 }
 
