9941a30725e01e123d4b8bf0ebec67f491df19a8
Eliud <58745656+eliudio@users.noreply.github.com>
2021-11-24 20:57:47 +0000

    Bugfixes

--- lib/editors/assignment_view_component_editor.dart
+++ lib/editors/assignment_view_component_editor.dart
@@ -1,20 +1,21 @@
 import 'package:eliud_core/style/frontend/has_dialog.dart';
 import 'package:eliud_core/tools/component/component_spec.dart';
 import 'package:flutter/material.dart';
+import 'package:eliud_core/core/blocs/access/access_bloc.dart';
 
 class AssignmentViewComponentEditorConstructor extends ComponentEditorConstructor {
   @override
   void updateComponent(BuildContext context, model, EditorFeedback feedback) {
-    openErrorDialog(context, title: 'Problem', errorMessage: 'No editor for this component available yet');
+    openErrorDialog(context, AccessBloc.currentAppId(context) + '/_error', title: 'Problem', errorMessage: 'No editor for this component available yet');
   }
 
   @override
   void createNewComponent(BuildContext context, EditorFeedback feedback) {
-    openErrorDialog(context, title: 'Problem', errorMessage: 'No editor for this component available yet');
+    openErrorDialog(context, AccessBloc.currentAppId(context) + '/_error', title: 'Problem', errorMessage: 'No editor for this component available yet');
   }
 
   @override
   void updateComponentWithID(BuildContext context, String id, EditorFeedback feedback) {
-    openErrorDialog(context, title: 'Problem', errorMessage: 'No editor for this component available yet');
+    openErrorDialog(context, AccessBloc.currentAppId(context) + '/_error', title: 'Problem', errorMessage: 'No editor for this component available yet');
   }
 }
