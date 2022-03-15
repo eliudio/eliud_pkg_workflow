2bccd939699caa4d12a8b69960993579bdd53e7c
Eliud <58745656+eliudio@users.noreply.github.com>
2022-03-15 21:43:58 +0000

    PolicyPresentationComponentEditorConstructor

--- lib/model/workflow_component_selector.dart
+++ lib/model/workflow_component_selector.dart
@@ -116,35 +116,32 @@ class _SelectWorkflowWidgetState extends State<SelectWorkflowWidget> {
   Widget build(BuildContext context) {
     return BlocBuilder<WorkflowListBloc, WorkflowListState>(
         builder: (context, state) {
-      if (state is WorkflowListLoading) {
-        return progressIndicator(widget.app, context);
-      } else if (state is WorkflowListLoaded) {
-        if (state.values == null) {
-          return text(widget.app, context, 'No items');
-        } else {
-          var children = <Widget>[];
-          children.add(Container(
-              height: widget.height - 45,
-              child: theList(
-                context,
-                state.values!,
-              )));
-          children.add(Column(children: [
-            divider(widget.app, context),
-            Center(
-                child: iconButton(widget.app, 
+      var children = <Widget>[];
+      if ((state is WorkflowListLoaded) && (state.values != null)) {
+        children.add(Container(
+            height: widget.height - 45,
+            child: theList(
               context,
-              onPressed: () {
-                widget.editorConstructor.createNewComponent(widget.app, context, (_) {});
-              },
-              icon: Icon(Icons.add),
-            ))
-          ]));
-          return ListView(
-              physics: ScrollPhysics(), shrinkWrap: true, children: children);
-        }
+              state.values!,
+            )));
+      } else {
+        children.add(Container(
+            height: widget.height - 45,
+            ));
       }
-      return Text("nothing");
+      children.add(Column(children: [
+        divider(widget.app, context),
+        Center(
+            child: iconButton(widget.app, 
+          context,
+          onPressed: () {
+            widget.editorConstructor.createNewComponent(widget.app, context, (_) {});
+          },
+          icon: Icon(Icons.add),
+        ))
+      ]));
+      return ListView(
+          physics: ScrollPhysics(), shrinkWrap: true, children: children);
     });
   }
 }
